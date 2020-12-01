0cac0333ebecd391da311a685a4250a8675344c6
Nacho Marin <nachomarineiroa91@gmail.com>
2020-12-01 15:23:57 +0100

    Change plot colormap. Add circles linestyle

--- plot_func.py
+++ plot_func.py
@@ -3,14 +3,19 @@ import matplotlib.pyplot as plt
 def plot_circles(ax, center, radius):
     ax.plot(center[0], center[1],'r+')
     for i in range(1,5):
-        circle = plt.Circle(center, radius=i*radius, fill=False, color='k', alpha=0.4)
+        circle = plt.Circle(center, 
+                            radius=i*radius, 
+                            fill=False, 
+                            color='k', 
+                            alpha=0.6,
+                            linestyle='--')
         ax.add_artist(circle)
 
 def plot_scatter_coordinates(df, x, y, z, center='', radius=''):
     """
     Plots a 2D scatter 
     """
-    ax = df.plot.scatter(x, y, c=z, colormap='hsv')
+    ax = df.plot.scatter(x, y, c=z, colormap='Paired', alpha=0.4)
     if center and radius:
         plot_circles(ax, center, radius)
     ax.plot()
