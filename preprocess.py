c0c324b170d67f573a9f92ed90a855fe649e43d0
Nacho Marin <nachomarineiroa91@gmail.com>
2020-12-04 01:07:18 +0100

    Added fill the gaps logic (skeleton df + merge)

--- preprocess.py
+++ preprocess.py
@@ -18,6 +18,8 @@ def filter_df_perimeter(df, lat_lim, lon_lim):
     filter_df.reset_index(inplace=True)
     return filter_df
 
+## TODO: instead of defining quadrant by nº of splits, consider giving the distance directly (step)
+## wbhich should be much easier  ()
 def define_quadrant(df, split, lat_lim, lon_lim):
     """
     Divides the map in a split by split grid. 
@@ -44,39 +46,65 @@ def aggregate_by_quadrant(df):
 
 def groupby_quadrant(df):
     group_cols = [df.Q1, df.Q2]
-    return df.groupby(group_cols)
+    gp = df.groupby(group_cols)
+    return gp, gp.groups.keys()
 
-def _fake_date_df(min_date, max_date):
+def cumsum_by_group(df, group='Date',cumsum_col='Gap'):
+    df['Cumsum'] = df.groupby(group)[cumsum_col].transform(pd.Series.cumsum) / df[cumsum_col]
+
+def extend_df(df, group='Date', multiplier=50):
+    """
+    Given a Series containing int, the line extends the df it´sclea corresponding value.
+    """
+    df['Gap'] = multiplier
+    df = pd.DataFrame(np.repeat(df.values,df['Gap'],axis=0), columns=df.columns)
+    cumsum_by_group(df, group, 'Gap')
+    return df
+
+def create_date_spam_df(min_date, max_date):
+    """
+    Given 2 dates limits, the function returns as many lines as timedeltas between both values.
+    Each line contains each date step
+    """
     delta_h = int((max_date - min_date).total_seconds() / 3600)
     dates = [min_date + datetime.timedelta(hours=i) for i in range(int(delta_h))]
     return pd.DataFrame(dates, columns=['Date'])
 
-def fill_date_gaps(df, min_date, max_date):
-    date_df = _fake_date_df(min_date, max_date)
-    merged_df = pd.merge(left = date_df,
-                        right = df,
-                        on = 'Date',
-                        how = 'left')
+def create_df_skeleton(min_date, max_date, split):
+    date_df = create_date_spam_df(min_date, max_date)
+    ext_1 = extend_df(date_df,group='Date',multiplier=split)
+    ext_1.rename(columns={'Cumsum':'Q1'}, inplace=True)
+    ext_2 = extend_df(ext_1, group=['Date','Q1'],multiplier=split)
+    ext_2.rename(columns={'Cumsum':'Q2'}, inplace=True)
+    ext_2.drop(columns='Gap', inplace=True)
+    ## TODO: we need to create a back function that given coordinate boundries, and the split
+    ## can associate the center coordinate lat and lon (inversa as define quadrant)
+    ## TODO: alternatively, we can create the skeleton coordinates giving boundry limits
+    return ext_2
+
+def fill_date_gaps(df, min_date, max_date, split):
+    skeleton_df = create_df_skeleton(min_date, max_date, split)
+    merged_df = pd.merge(left = skeleton_df,
+                         right = df,
+                         on = ['Date', 'Q1', 'Q2'],
+                         how = 'left')
     merged_df.reset_index(inplace=True, drop=True)
     ## TODO: function this fillna
     merged_df['Demand'].fillna(0, inplace=True)
+    ## TODO: 
     merged_df['LatQ'].fillna(df.LatQ.max(), inplace=True)
     merged_df['LonQ'].fillna(df.LonQ.max(), inplace=True)
-    merged_df['Q1'].fillna(df.Q1.max(), inplace=True)
-    merged_df['Q2'].fillna(df.Q2.max(), inplace=True)
 
     return merged_df
 
-## TODO: fill quadrant gaps
-
-def fill_all_quadrants(gp, min_date, max_date):
+def fill_all_quadrants(df_list, gp_keys, min_date, max_date, split):
     """
-    Given a pandas df groupby object, it fullfills all the missing date/hours for each
+    Given a pandas groupby object, it fullfills all the missing date/hours for each
     group and concatenates the final results
     """
-    df_list = [fill_date_gaps(df, min_date, max_date) for _, df in gp]  
-    new_df = pd.concat(df_list)
-    new_df.reset_index(inplace=True, drop=True)
+    df_to_concat = [fill_date_gaps(df, min_date, max_date, split) for df in df_list] 
+    new_df = pd.concat(df_to_concat)
+    new_df.reset_index(inplace=splitTrue, drop=True)
     return new_df
 
 if __name__ == '__main__':
@@ -92,11 +120,4 @@ if __name__ == '__main__':
     df = filter_df_perimeter(df, params['lat_lim'], params['lon_lim'])
     define_quadrant(df, params['split'], params['lat_lim'], params['lon_lim']) 
     df = aggregate_by_quadrant(df)
-
-    ### Add missing lines
-    gp = groupby_quadrant(df)
-    test_gp = gp.get_group((17,40))
-    clean_df = fill_all_quadrants(gp, min_date, max_date)
-
-
-    
+    complete_df = fill_date_gaps(df, min_date, max_date, params['split'])
