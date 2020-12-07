d35dcec7d6d44ceb6eebf6e28cd159caec9ae2a5
Nacho Marin <nachomarineiroa91@gmail.com>
2020-12-07 11:11:23 +0100

    Add get_files_function

--- dataloader.py
+++ dataloader.py
@@ -1,5 +1,12 @@
+import os
 import pandas as pd
 
+def get_files_path(path):
+    """
+    Returns a list of tuples containing the file name and the file path
+    """
+    path_tuple_list = [(f, os.path.join(path, f)) for f in os.listdir(path) if not f.startswith('.')]
+    return path_tuple_list
 
 def _format_df_date(df):
     df.rename(columns={'Date/Time':'Date'}, inplace = True)
