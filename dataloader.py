6d426c36ddfb28dbadab4f5e12ccb2462c2c45bd
Nacho Marin <nachomarineiroa91@gmail.com>
2020-12-10 00:27:25 +0100

    Improve data formatting performance
    date_cols attribute  inside read_csv is insanely slow (several minutes). By formatting the dates predefining the format using pd.to_datetime makes the whole parsing process to be as short as 7-8 seconds

--- dataloader.py
+++ dataloader.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import os
 import pandas as pd
 
@@ -8,15 +9,20 @@ def get_files_path(path):
     path_tuple_list = [(f, os.path.join(path, f)) for f in os.listdir(path) if not f.startswith('.')]
     return path_tuple_list
 
-def _format_df_date(df):
-    df.rename(columns={'Date/Time':'Date'}, inplace = True)
+def _format_date(df):
+    df = df.rename(columns={'Date/Time':'Date'})
+    df['Date'] = pd.to_datetime(df['Date'], format='%m/%d/%Y %H:%M:%S')
     df = df.assign(Date=df.Date.dt.round('H'))
-    
     return df
 
-def parse_df(file_path, date_cols=['Date/Time'],headers=['Date/Time', 'Lat', 'Lon']):
-    df = pd.read_csv(file_path, parse_dates=date_cols, usecols=headers)
+def parse_df(file_path,headers=['Date/Time', 'Lat', 'Lon']):
+    then = datetime.now()
+    print('* Start parse df')
+    df = pd.read_csv(file_path, usecols=headers)
+    print(f' - File read ({datetime.now() - then})')
     df['Demand'] = 1
-    df = _format_df_date(df)
-    print(f'* Data parsed: {file_path}')
+    print(f' - Demand = 1 added ({datetime.now() - then})')
+    df = _format_date(df)
+    print(f' - Dates reformated ({datetime.now() - then})')
+    print(f' - Data parsed: {file_path}')
     return df
