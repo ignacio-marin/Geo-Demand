418fe4d49293807df61965ebfc725d32513d7f9f
Nacho Marin <nachomarineiroa91@gmail.com>
2021-01-12 01:07:02 +0100

    Complete GeoModel

--- model.py
+++ model.py
@@ -10,49 +10,62 @@ from settings import ACCOUNTS
 
 ### TODO: self.filter/transformed dataframe
 
+class Point:
+    
+    def __init__(self,center):
+        self.center =  center
+        self.predictions = {}
+        
+
 class GeoModel:
 
-    def __init__(self, center:tuple, df:pd.DataFrame, r:float, r_decay:float, alpha:float):
-        self.center_tuple = center
+    def __init__(self, df:pd.DataFrame, center:tuple, r:float, r_decay:float, alpha:float):
+        self.center = center
         self.df = df
-        self.transformed_df = df.copy()
-        self.radius = r
         self.decay = r_decay
+        self.radius = r
+        self.r_lst = radius_list(self.radius, self.decay)
         self.alpha = alpha
-        self.r_lst = radius_list(self.r, self.decay)
+        self.fltr_df = self.__filter_df(self.df, 'LatQ', 'LonQ') 
+        ## Column names should be defined in the client settings (also used in preprocess)
 
-    def filter_df(self, self.df):
-        self.transformed_df.loc[:,'Distance'] = euclidian_distance(self.center)
-        self.transformed_df = df[df.Distance <= self.r_lst[-1]].reset_index(drop=True)
+    def __filter_df(self, df, lat_col:str, lon_col:str):
+        fltr_df = self.df.copy()
+        latitude = fltr_df.loc[:,lat_col]
+        longitude = fltr_df.loc[:,lon_col]
+        fltr_df.loc[:,'Distance'] = self.euclidian_distance(self.center, latitude, longitude)
+        fltr_df = fltr_df[fltr_df.Distance <= max(self.r_lst)].reset_index(drop=True)
+        return fltr_df
 
 ### Distance
     @staticmethod   
-    def euclidian_distance(center:tuple):
+    def euclidian_distance(center:tuple, latitude:pd.Series, longitude:pd.Series):
         xi, yi = center
-        return np.sqrt((df.loc[:,'LatQ'] - xi)**2 + (df.loc[:,'LonQ'] - yi)**2)
+        return np.sqrt((latitude - xi)**2 + (longitude - yi)**2)
 
     @staticmethod
-    def relative_distance(radius_arr:list, d_column: str):
+    def relative_distance(distance:pd.Series, radius_arr:list):
         """
         Given an array of radius, it classifies each point relative to it.
         Each points euclidean distance will be compared with the radius list and
         returned the index (relative distance) it falls in. 
         """
-        return df.loc[:,d_column].apply(lambda x: 1 + np.searchsorted(radius_arr, x))
+        return distance.apply(lambda x: 1 + np.searchsorted(radius_arr, x))
 
     @staticmethod
-    def time_gap(date_serie:pd.Series, agg='weekly'):
+    def time_gap(dates:pd.Series, agg='weekly'):
         """
         Returns the relative time gap from the latest date. The agg arguments defines
         the gap measure. By defaul, is per hour
         """
         uom = (3600*24*7) if agg == 'weekly' else 3600 ## can be improved
-        max_Date = date_serie.max()
-        time_delta = max_Date - date_serie
+        max_Date = dates.max()
+        time_delta = max_Date - dates
         return 1 + np.floor(time_delta.dt.total_seconds() / uom)
 
 ### Weights
-    def _inverse_weight(self,v_series:pd.Series, alfa:float, inv_split = ''):
+    @staticmethod
+    def inverse_weight(values:pd.Series, alfa:float, inv_split = ''):
         """
         Unique inv_split gives the same weight to each unique value. 
         By default, it divides the inverse value among all data points
@@ -66,45 +79,55 @@ class GeoModel:
         + 4 |   0.25  | 0.25/1.75 = 0.142 |  0.25/3.25 = 0.076 |
         ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         """
-        inv_distance = (1 / v_series ** alfa)
+        inv_distance = (1 / values ** alfa)
         if inv_split == 'unique':
             return inv_distance / inv_distance.unique().sum()
         else:
             return inv_distance / inv_distance.sum()
 
-    def time_weights(self,self.df:pd.DataFrame,d_column:str,grouping:list, self.alfa:float):
+    def time_weights(self,grouping:list):
         """
         Grouping shpuld be: ['WeekDay','Hour'] 
         """
-        self.df.loc[:,d_column] = time_gap(self.df.Date)
-        group = self.df.groupby(grouping)
-        return group[d_column].transformd(_inverse_weight, self.alfa, inv_split='unique')
+        self.fltr_df.loc[:,'TimeGap'] = self.time_gap(self.fltr_df.Date)
+        group = self.fltr_df.groupby(grouping)
+        return group['TimeGap'].transform(self.inverse_weight, self.alpha, inv_split='unique')
         
-    def distance_weights(self,self.df:pd.DataFrame, d_column:str,grouping:list, self.alfa:float):
+    def distance_weights(self,grouping:list):
         """
         Grouping should be: ['Date'] (includes hour)
         Recommended alfa: 1.5-2
         """
-        self.df.loc[:,d_column]  = relative_distance(self.r_lst, 'Distance')
-        group = self.df.groupby(grouping)
-        return group[d_column].transform(_inverse_weight, self.alfa)
+        self.fltr_df.loc[:,'RelativeDistance'] = self.relative_distance(self.fltr_df['Distance'], self.r_lst)
+        group = self.fltr_df.groupby(grouping)
+        return group['RelativeDistance'].transform(self.inverse_weight, self.alpha)
 
 ### Calculate model
-    def run():
-        pass
+    def get_points_probs(self):
+        time_group = [self.fltr_df.Date.dt.dayofweek, self.fltr_df.Date.dt.hour]
+        dist_group = [self.fltr_df.Date.dt.dayofweek, self.fltr_df.Date.dt.hour, self.fltr_df.Date]
+        self.fltr_df.loc[:,'TimeW'] = self.time_weights(time_group)
+        self.fltr_df.loc[:,'DistanceW'] = self.distance_weights(dist_group)
+        self.fltr_df.loc[:,'Prob'] = self.fltr_df.DistanceW * self.fltr_df.TimeW
+
+    def get_distribution(self, weekday:int, hour:int):
+        self.get_points_probs()
+
+        filter1 = (self.fltr_df.Date.dt.dayofweek == weekday)
+        filter2 = (self.fltr_df.Date.dt.hour == hour)
+        sub_df = self.fltr_df[filter1 & filter2]
+        return sub_df.groupby('Demand')['Prob'].sum()
 
 if __name__ == '__main__':
     fh = FileHandler('uber')
     df_lst = [pd.read_csv(path[1]) for path in fh.get_dir_files('clean')]
     df = pd.concat(df_lst).reset_index(drop=True)
+    ### Date format should be hanldled in the DataLoader class TODO
     df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d %H:%M:%S')
-    r_lst = radius_list(0.004, 0.1)
-
+    r = 0.004
+    r_decay = 0.1
+    alpha = 1.5
     center = ACCOUNTS['uber']['center']
-    df.loc[:,'Distance'] = euclidian_distance(center)
-    df = df[df.Distance <= r_lst[-1]].reset_index(drop=True)
-    # df.loc[:,'RelativeDistance']  = relative_distance(r_lst, 'Distance')
-    df['TimeW'] = time_weights(df,'TimeW', [df.Date.dt.dayofweek, df.Date.dt.hour], 1.5)
-    df['DistanceW'] = distance_weights(df,'RelativeDistance', [df.Date.dt.dayofweek, 
-                                                               df.Date.dt.hour, df.Date], 1.5)
+    print('* Data parsed')
+    gm = GeoModel(df, center,r ,r_decay, alpha)
 
