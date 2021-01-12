272a0833d2b021f77d7f1e4586706917999fa2f4
Nacho Marin <nachomarineiroa91@gmail.com>
2021-01-12 02:07:24 +0100

    Add fill_series_gaps

--- helpers.py
+++ helpers.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 ### Equal area rings approach
 def ring_equal_area_radius(r, area):
@@ -43,3 +44,23 @@ def semivariance(x):
         z = np.reshape(x, (len(x), 1))
         v = 0.5 * (z - z.transpose()) ** 2
         return sum(np.concatenate(v)) / len(x)
+
+### Normalize array and fill the gaps
+
+def fill_series_gaps(x:pd.Series):
+    """
+    Gets a pandas Series with missing indexes, fills the spaces
+    and interpolates the values between them.
+    Because the values are probabilities, it recalculates the final
+    value over the total
+    +++++++++++++++++++++
+    | Index |    Value  |    fill+interpolation: 
+    +++++++++++++++++++++               [0.4, 0.4, 0.3, 0.2]
+    +   0   |     0.4   |    final:
+    +   1   |     0.4   |             [0.307, 0.307, 0.23, 0.153]
+    +   4   |     0.2   |
+    """
+    n = x.index.max()
+    y = pd.Series(index=np.arange(x.index.max()+1),dtype=np.float64)
+    fill = x.combine(y, min).interpolate()
+    return fill / fill.sum()
