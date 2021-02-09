a154ce50818ee6817da3ada63438552c8e4d2253
Ignacio Marin <ignacio.marin.eiroa@gmail.com>
2021-02-09 02:36:26 +0100

    Change center coordinates

--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@ ACCOUNTS = {
         'lon_lim': (-74.1,-73.9),
         'lat_lim': (40.65,40.85), 
         'step': 0.005,
-        'center': (-73.9975, 40.75),
+        'center': (-73.99, 40.75),
         'radius': 0.004,
         'r_decay':0.1,
         'alpha':1.5
